90c640a32475d11ea266a6362366a1df4b7f331a
SyedAzeem24 <azeemthegamer146@gmail.com>
2026-07-20 10:30:12 +0500

    configured for deployed environment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://gnss-virtual-lab-backend.onrender.com",
+    baseURL: "https://gnss-virtual-lab-backend.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
